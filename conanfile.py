ec46972a1ac604449c431f8d87cd25cbc43183f2
Arnor Nolen <andrey1gorbulin@gmail.com>
2023-09-06 20:34:50 +0300

    Presets

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,21 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout, CMakeToolchain
+from conan.tools.cmake import cmake_layout, CMake, CMakeDeps, CMakeToolchain
 
 
 class CppTemplate(ConanFile):
+    name = "CppTemplate"
+    version = "1.0"
+    package_type = "application"
+
+    license = "MIT"
+    author = "Arnor Nolen"
+    url = "https://github.com/arnor-nolen"
+    description = "C++ template."
+    topics = ("C++", "Conan", "Template")
+
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps"
+    exports_sources = "CMakeLists.txt", "src/*"
+
     default_options = {"fmt/*:shared": True}
 
     def requirements(self):
@@ -13,10 +24,20 @@ class CppTemplate(ConanFile):
         self.requires("fmt/10.1.1")
 
     def generate(self):
-        # Configure CMakeToolchain
+        deps = CMakeDeps(self)
+        deps.generate()
         tc = CMakeToolchain(self)
         tc.user_presets_path = "ConanPresets.json"
         tc.generate()
 
     def layout(self):
         cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
